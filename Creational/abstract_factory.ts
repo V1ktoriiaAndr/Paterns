7be027278c125a60dac8a32070b80db3dfc4b719
Viktoriia Andrusyk <andrusyk.viktoriia@chnu.edu.ua>
2026-03-12 10:11:51 +0200

    Fixed abstract factory

--- Creational/abstract_factory.ts
+++ Creational/abstract_factory.ts
@@ -29,46 +29,81 @@ class Bow implements Weapon{
     }
 }
 
-interface MobFactory{
-    createMob(): Mob;
-    createWeapon(): Weapon;
-}
+abstract class MobFactory {
+    abstract createMob(): Mob;
 
-class ZombieFactory implements MobFactory{
-    createMob(): Mob {
-        return new Zombie();
-    }
-    createWeapon(): Weapon {
-        return new Sword();
-    }
-}
+    abstract createWeapon(): Weapon;
 
-class SkeletonFactory implements MobFactory{
-    createMob(): Mob {
-        return new Skeleton();
-    }
-    createWeapon(): Weapon {
-        return new Bow();
+    spawnCombatant(): { mob: Mob; weapon: Weapon } {
+        return {
+            mob: this.createMob(),
+            weapon: this.createWeapon(),
+        };
     }
 }
 
-class Game{
+class ZombieFactory extends MobFactory {
+    createMob(): Mob     { return new Zombie(); }
+    createWeapon(): Weapon { return new Sword(); }
+}
+
+class SkeletonFactory extends MobFactory {
+    createMob(): Mob     { return new Skeleton(); }
+    createWeapon(): Weapon { return new Bow(); }
+}
+
+type MobType = 'zombie' | 'skeleton';
+
+class MobSuperFactory {
+    private factories: Record<MobType, () => MobFactory> = {
+        zombie:   () => new ZombieFactory(),
+        skeleton: () => new SkeletonFactory(),
+    };
+
+    create(type: MobType): MobFactory {
+        const factory = this.factories[type];
+        if (!factory) throw new Error(`Unknown mob type: ${type}`);
+        return factory();
+    }
+
+    spawnCombatant(type: MobType): { mob: Mob; weapon: Weapon } {
+        return this.create(type).spawnCombatant();
+    }
+}
+
+class Game {
     private mob: Mob;
     private weapon: Weapon;
 
-    constructor(private mobFactory: MobFactory){
-        this.mob = mobFactory.createMob();
-        this.weapon = mobFactory.createWeapon();
+    constructor(private superFactory: MobSuperFactory, private mobType: MobType) {
+        const { mob, weapon } = superFactory.spawnCombatant(mobType);
+        this.mob    = mob;
+        this.weapon = weapon;
     }
 
-    battle(){
+    battle() {
         this.mob.live();
         this.weapon.attack();
     }
+
+    switchMob(newType: MobType) {
+        const { mob, weapon } = this.superFactory.spawnCombatant(newType);
+        this.mob     = mob;
+        this.weapon  = weapon;
+        this.mobType = newType;
+    }
 }
 
-const game = new Game(new ZombieFactory());
+const superFactory = new MobSuperFactory();
+
+const game = new Game(superFactory, 'zombie');
 game.battle();
 console.log('-----------------------------');
-const game2 = new Game(new SkeletonFactory());
+
+const game2 = new Game(superFactory, 'skeleton');
 game2.battle();
+console.log('-----------------------------');
+
+game.switchMob('skeleton');
+game.battle();
+
